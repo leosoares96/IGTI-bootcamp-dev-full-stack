19d2da53980620872e0e222a176810155d2ac18d
Leonardo Soares <leo.soares64@outlook.com>
2020-07-13 20:32:21 -0300

    Trabalho pratico de simulador RGB

---
+++ CAP01/trabalho-pratico/js/script.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', start);
+
+function start(){
+  var [inputRed, inputGreen, inputBlue] = document.querySelectorAll("input[type='range'");
+  var [numberRed, numberGreen, numberBlue] = document.querySelectorAll("input[type='text'");
+  
+  updateColor();
+
+  inputRed.addEventListener('input',updateColor);
+  inputGreen.addEventListener('input',updateColor);
+  inputBlue.addEventListener('input',updateColor);
+
+  function updateColor(){
+    numberRed.value = inputRed.value;
+    numberGreen.value = inputGreen.value;
+    numberBlue.value = inputBlue.value;
+    
+    var boxColor = document.querySelector('#boxColor');
+    var body = document.querySelector('body');
+    boxColor.style.background = `rgb(${numberRed.value},${numberGreen.value},${numberBlue.value})`;
+  }
+}
+
