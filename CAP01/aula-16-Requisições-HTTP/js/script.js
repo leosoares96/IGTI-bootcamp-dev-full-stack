2df1ee0d8ec0bd11826fd8fc0be230f4efc8244a
Leonardo Soares <leo.soares64@outlook.com>
2020-07-17 16:37:17 -0300

    async await

--- CAP01/aula-16-Requisições-HTTP/js/script.js
+++ CAP01/aula-16-Requisições-HTTP/js/script.js
@@ -1,7 +1,39 @@
 window.addEventListener('load', function () {
-  fetch('https://api.github.com/users/leosoares96').then((res) => {
-    res.json().then((data) => {
-      console.log(data);
+  doFetch();
+  doFetchAsync();
+
+  divisionPromise(7, 2)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((e) => {
+      console.log(e);
     });
-  });
 });
+
+function divisionPromise(a, b) {
+  return new Promise((resolve, reject) => {
+    if (b === 0) {
+      reject('Não é possível dividir por zero');
+    }
+    resolve(a / b);
+  });
+  return a / b;
+}
+
+function doFetch() {
+  fetch('https://api.github.com/users/leosoares96')
+    .then((res) => {
+      res.json().then((data) => {
+        console.log(data);
+      });
+    })
+    .catch((error) => {
+      console.error('Erro na requisição: ' + error);
+    });
+}
+
+async function doFetchAsync() {
+  const result = await fetch('https://api.github.com/users/leosoares96');
+  console.log(result);
+}
