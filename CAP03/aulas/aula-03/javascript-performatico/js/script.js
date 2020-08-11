023ffd781a2388dffca25d98e0cdab9f37484469
Leonardo Soares <leo.soares64@outlook.com>
2020-08-10 23:07:17 -0300

    trabalhando com react

---
+++ CAP03/aulas/aula-03/javascript-performatico/js/script.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', start);
+
+const clickArray = [];
+
+function start() {
+  const button = document.querySelector('#clickButton');
+  button.addEventListener('click', handleButtonClick);
+}
+function handleButtonClick() {
+  const item = getNewTimestamp();
+  clickArray.push(item);
+
+  render(item);
+}
+function render(item) {
+  const ul = document.querySelector('#data');
+
+  const li = document.createElement('li');
+  li.textContent = item;
+  // ul.innerHTML = '';
+
+  // let lis = '';
+  // clickArray.map((item) => {
+  //   lis += `<li>${item}</li>`;
+  // });
+
+  // ul.innerHTML = lis;
+
+  ul.appendChild(li);
+  document.title = clickArray.length;
+}
---
+++ CAP03/aulas/aula-03/reac-hooks/src/helpers/dateTimeHelpers.js
@@ -0,0 +1,34 @@
+function letPad(value, count = 2, char = '0') {
+  let newValue = '';
+
+  if (value.toString().length < count) {
+    for (let i = 0; i < count - value.toString().length; i++) {
+      newValue += char;
+    }
+  }
+  newValue = newValue + value;
+  return newValue;
+}
+
+function getNewTimestamp() {
+  const now = new Date();
+  let result = '';
+
+  result += letPad(now.getDate());
+  result += '/';
+  result += letPad(now.getMonth() + 1);
+  result += '/';
+  result += now.getFullYear();
+  result += ' ';
+  result += letPad(now.getHours());
+  result += ':';
+  result += letPad(now.getMinutes());
+  result += ':';
+  result += letPad(now.getSeconds());
+  result += '.';
+  result += letPad(now.getMilliseconds(), 3);
+
+  return result;
+}
+
+export { getNewTimestamp };
