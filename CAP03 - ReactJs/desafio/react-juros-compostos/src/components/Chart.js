ada45632616b703aed0197e6c4af279a4af98c97
root <leo.soares64@outlook.com>
2020-08-22 20:00:38 -0300

    ok

---
+++ CAP03 - ReactJs/desafio/react-juros-compostos/src/components/Chart.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+export default function Chart({ calcs }) {
+  const data = {
+    labels: calcs.map((calc) => calc.id),
+    datasets: [
+      {
+        label: 'Montante',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: calcs.map((calc) => calc.acc),
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Bar
+        data={data}
+        width={100}
+        height={200}
+        options={{
+          maintainAspectRatio: false,
+        }}
+      />
+    </div>
+  );
+}
